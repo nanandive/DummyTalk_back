0f0aeb4d6dc190be4fbb3f614e05ddeab5208666
Choi Youngwook <duddnr3618@gmail.com>
2023-12-04 13:22:27 +0900

    23.12.04 초기셋팅 커밋

---
+++ src/main/java/com/example/DummyTalk/DummyTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.DummyTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyTalkApplication.class, args);
+    }
+
+}
