743e460e7d0c8d671106dc9631f185d079fc122a
Choi Youngwook <duddnr3618@gmail.com>
2023-12-13 14:47:50 +0900

    23.12.13 back commit

--- src/test/java/com/example/DummyTalk/DummyTalkApplicationTests.java
+++ src/test/java/com/example/DummyTalk/DummyTalkApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.DummyTalk;
 
+import com.example.DummyTalk.User.Entity.User;
+import com.example.DummyTalk.User.Repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class DummyTalkApplicationTests {
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
-    void contextLoads() {
+    void createUserTest() {
+        for (int i = 1; i <= 10; i++) {
+            User user = new User();
+            user.setNickname("user" + i);
+            user.setPassword("password" + i);
+            user.setUserEmail("user" + i + "@example.com");
+            user.setUserImgPath("/img/user" + i + ".jpg");
+            user.setUserName("User " + i);
+            user.setUserPhone("010-000" + i + "-000" + i);
+            user.setCreateAt(LocalDateTime.now());
+            user.setUpdateAt(LocalDateTime.now());
+
+            userRepository.save(user);
+        }
     }
 
 }
