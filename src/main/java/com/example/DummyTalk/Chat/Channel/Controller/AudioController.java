aa3458c79c0704ac1a94516f3e98e910e6d32cbd
heski0709 <heski0709@gmail.com>
2023-12-21 21:37:31 +0900

    MultipartFile을 FastApi에 보내기 위한 코드 추가

--- src/main/java/com/example/DummyTalk/Chat/Channel/Controller/AudioController.java
+++ src/main/java/com/example/DummyTalk/Chat/Channel/Controller/AudioController.java
@@ -3,10 +3,14 @@ package com.example.DummyTalk.Chat.Channel.Controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.MediaType;
@@ -18,12 +22,16 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.example.DummyTalk.Chat.Channel.Dto.SendChatDto;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/audio")
@@ -52,44 +60,55 @@ public class AudioController {
     }
 
     @PostMapping("/upload")
-    public void uploadAudio(@ModelAttribute MultipartFile file) {
-        String uploadPath = "C:\\Audio\\";
-        String fileName = UUID.randomUUID().toString() + ".wav";
+    public MessageResponse uploadAudio(@ModelAttribute MultipartFile file) {
+        CountDownLatch cdl = new CountDownLatch(1);
+        StringBuilder result = new StringBuilder();
 
         try {
             File temp = File.createTempFile(file.getOriginalFilename(), ".wav");
             // 파일 저장
             file.transferTo(new File(temp.getPath()));
+
             WebClient.create()
                     .post()
-                    .uri("http://localhost:8000/api/v1/audio/kor_Hand")  // FastAPI 엔드포인트 경로
+                    .uri("http://localhost:8000/api/v1/audio/text/kor_Hand") // FastAPI 엔드포인트 경로
                     .contentType(MediaType.MULTIPART_FORM_DATA)
                     .body(BodyInserters.fromMultipartData("file", new UrlResource(temp.toURI())))
                     .retrieve()
                     .bodyToMono(String.class)
-                    .subscribe(); 
-        } catch (IOException e) {
+                    .doOnTerminate(() -> cdl.countDown())
+                    .subscribe(res -> result.append(res));
+
+            cdl.await();
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+
+        SendChatDto chat = new SendChatDto();
+        chat.setMessage(result.toString());
+        chat.setType("AUDIO");
+
+        return new MessageResponse(null, "200", chat);
     }
 
     @PostMapping("/transport/{nationalLanguage}")
-    public ResponseEntity<?> translateTextToAudio(String text, @PathVariable String nationalLanguage) {
-        DataBuffer audioData = WebClient.create()
-                .post()
-                .uri("/receive-file")
-                .accept(MediaType.APPLICATION_OCTET_STREAM)
-                .retrieve()
-                .bodyToMono(DataBuffer.class)
-                .block();
-                // .asByteBuffer()
-                // .array();
+    public ResponseEntity<?> translateTextToAudio(@RequestBody SendChatDto chat,
+            @PathVariable String nationalLanguage) {
 
-        audioData.readableByteBuffers();
+        byte[] result = WebClient.create()
+                .post()
+                .uri("http://localhost:8000/api/v1/audio/audio/" + nationalLanguage)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(chat))
+                .retrieve()
+                .bodyToMono(byte[].class)
+                .block();
+
+
         // 리액트 클라이언트에게 바로 byte 배열을 응답
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(audioData);
+                .body(result);
     }
-    
+
 }
