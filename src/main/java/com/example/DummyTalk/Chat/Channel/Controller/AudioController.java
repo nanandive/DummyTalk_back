e80c5b43f96ed5734f1e6a77df8bc86fed9dba99
heski0709 <heski0709@gmail.com>
2023-12-21 14:46:52 +0900

    audio 전송 코드 추가

--- src/main/java/com/example/DummyTalk/Chat/Channel/Controller/AudioController.java
+++ src/main/java/com/example/DummyTalk/Chat/Channel/Controller/AudioController.java
@@ -5,17 +5,24 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @RestController
@@ -31,7 +38,7 @@ public class AudioController {
     @MessageMapping("/audioMessage")
     @SendTo("/topic/audio/{channelId}")
     public void receiveAudio(@Payload byte[] data) {
-        
+
         String directoryPath = "C:\\Audio";
         Path path = Paths.get(directoryPath, "audioFile.wav");
         try (FileOutputStream out = new FileOutputStream(path.toFile())) {
@@ -45,16 +52,44 @@ public class AudioController {
     }
 
     @PostMapping("/upload")
-    public void postMethodName(@ModelAttribute MultipartFile file) {
-        String uploadPath = "C:\\Audio";
-        String fileName = file.getOriginalFilename();
+    public void uploadAudio(@ModelAttribute MultipartFile file) {
+        String uploadPath = "C:\\Audio\\";
+        String fileName = UUID.randomUUID().toString() + ".wav";
 
         try {
+            File temp = File.createTempFile(file.getOriginalFilename(), ".wav");
             // 파일 저장
-            file.transferTo(new File(uploadPath + fileName));
+            file.transferTo(new File(temp.getPath()));
+            WebClient.create()
+                    .post()
+                    .uri("http://localhost:8000/api/v1/audio/kor_Hand")  // FastAPI 엔드포인트 경로
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(BodyInserters.fromMultipartData("file", new UrlResource(temp.toURI())))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .subscribe(); 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @PostMapping("/transport/{nationalLanguage}")
+    public ResponseEntity<?> translateTextToAudio(String text, @PathVariable String nationalLanguage) {
+        DataBuffer audioData = WebClient.create()
+                .post()
+                .uri("/receive-file")
+                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .retrieve()
+                .bodyToMono(DataBuffer.class)
+                .block();
+                // .asByteBuffer()
+                // .array();
+
+        audioData.readableByteBuffers();
+        // 리액트 클라이언트에게 바로 byte 배열을 응답
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(audioData);
     }
     
 }
--- src/main/java/com/example/DummyTalk/Chat/Channel/Service/ChatServiceImpl.java
+++ src/main/java/com/example/DummyTalk/Chat/Channel/Service/ChatServiceImpl.java
@@ -1,5 +1,16 @@
 package com.example.DummyTalk.Chat.Channel.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import com.example.DummyTalk.Chat.Channel.Controller.MessageResponse;
 import com.example.DummyTalk.Chat.Channel.Dto.ChannelParticipantDto;
 import com.example.DummyTalk.Chat.Channel.Dto.MessageHistoryDto;
@@ -14,21 +25,9 @@ import com.example.DummyTalk.Exception.ChatFailException;
 import com.example.DummyTalk.User.DTO.ChatSenderDTO;
 import com.example.DummyTalk.User.Entity.User;
 import com.example.DummyTalk.User.Repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DataAccessException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.io.File;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -138,15 +137,27 @@ public class ChatServiceImpl implements ChatService {
     /* Chat 번역 */
     public MessageResponse translateMessage(SendChatDto chat, String nationLanguage) {
 
-        MessageResponse response = WebClient.create()
+        CountDownLatch cdl = new CountDownLatch(1);
+        MessageResponse response = new MessageResponse();
+        WebClient.create()
                 .post()
                 .uri("http://localhost:8000/api/v1/trans/" + nationLanguage)
                 .header("Content-Type", "application/json")
                 .body(BodyInserters.fromValue(chat))
                 .retrieve()
                 .bodyToMono(MessageResponse.class)
-                .block();
+                .doOnTerminate(() -> cdl.countDown())
+                .subscribe((res) -> response.setMessageResponse(res.getNickname(), res.getStatus(), res.getChat()));
+        
+
         log.info("{}", response);
+
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return response;
     }
 
