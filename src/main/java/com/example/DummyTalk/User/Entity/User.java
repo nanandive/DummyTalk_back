39c1622759d1c7fc8e2806dc25604a1e5e75ae6d
heski0709 <heski0709@gmail.com>
2023-12-24 02:47:54 +0900

    ddl-auto 설정이 create가 아닐 경우 user 생성하지 않도록 수정

--- src/main/java/com/example/DummyTalk/User/Entity/User.java
+++ src/main/java/com/example/DummyTalk/User/Entity/User.java
@@ -1,6 +1,5 @@
 package com.example.DummyTalk.User.Entity;
 
-
 import com.example.DummyTalk.Common.Entity.BaseTimeEntity;
 import com.example.DummyTalk.User.DTO.UserDTO;
 import com.example.DummyTalk.User.Repository.UserRepository;
@@ -10,12 +9,9 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.annotation.CreatedDate;
 import lombok.*;
@@ -34,7 +30,6 @@ import java.util.List;
 @Setter
 @Builder
 public class User {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -56,7 +51,7 @@ public class User {
     @Column(name = "user_phone")
     private String userPhone;
 
-    @Column(name= "credential", length = 500)
+    @Column(name = "credential", length = 500)
     private String credential;
 
     @Column(name = "user_img_path")
@@ -82,18 +77,23 @@ public class User {
     @RequiredArgsConstructor
     public static class UserInit implements CommandLineRunner {
         private final UserRepository userRepository;
-
         private final UserService userService;
 
+        @Value("${spring.jpa.hibernate.ddl-auto}")
+        private String DDL_AUTO_SETTING;
+
         @Override
-        public void run(String... args) throws Exception {
+        pu
+blic void run(String... args) throws Exception {
+            if (!DDL_AUTO_SETTING.equals("create"))
+                return;
             for (int i = 1; i <= 3; i++) {
                 UserDTO userDTO = UserDTO.builder()
-                        .name("유저"+i)
-                        .userEmail(i+"test@test.com")
+                        .name("유저" + i)
+                        .userEmail(i + "test@test.com")
                         .password("1234")
-                        .nickname("유저"+i)
-                        .userPhone("123"+i)
+                        .nickname("유저" + i)
+                        .userPhone("123" + i)
                         .build();
 
                 userService.signUp(userDTO);
