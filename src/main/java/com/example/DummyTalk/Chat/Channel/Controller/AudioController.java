cb349cfdfa5adc5b786389de22028d27e8e1d82e
heski0709 <heski0709@gmail.com>
2023-12-26 17:29:09 +0900

    오디오와 텍스트 웹소켓 경로 분리

--- src/main/java/com/example/DummyTalk/Chat/Channel/Controller/AudioController.java
+++ src/main/java/com/example/DummyTalk/Chat/Channel/Controller/AudioController.java
@@ -5,11 +5,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -36,93 +37,85 @@ public class AudioController {
         this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/audioMessage")
+    @MessageMapping("/{channelId}/audio")
     @SendTo("/topic/audio/{channelId}")
-    public void receiveAudio(@Payload byte[] data) {
+    public MessageResponse receiveAudio(SendChatDto message
+            , @DestinationVariable String channelId) {
 
-        String directoryPath = "C:\\Audio";
-        Path path = Paths.get(directoryPath, "audioFile.wav");
-        try (FileOutputStream out = new FileOutputStream(path.toFile())) {
-            out.write(data);
-            // String fileUrl = "http://localhost:9999/voice/audioFile.wav";
-            // messagingTemplate.convertAndSend("/topic/audioPath", fileUrl);
-        } catch (IOException e) {
-            e.printStackTrace();
-            // messagingTemplate.convertAndSend("/topic/audioError", "오류 발생: 오디오 파일 저장 실패");
-        }
+                return new MessageResponse(message.getNickname(), "200", message);
     }
 
-    // @PostMapping("/upload")
-    // public MessageResponse uploadAudio(@ModelAttribute MultipartFile file) {
-    //     CountDownLatch cdl = new CountDownLatch(1);
-    //     StringBuilder result = new StringBuilder();
-
-    //     try {
-    //         File temp = File.createTempFile(file.getOriginalFilename(), ".wav");
-    //         // 파일 저장
-    //         file.transferTo(new File(temp.getPath()));
-
-    //         WebClient.create()
-    //                 .post()
-    //                 .uri("http://localhost:8000/api/v1/audio/text/kor_Hand") // FastAPI 엔드포인트 경로
-    //                 .contentType(MediaType.MULTIPART_FORM_DATA)
-    //                 .body(BodyInserters.fromMultipartData("file", new UrlResource(temp.toURI())))
-    //                 .retrieve()
-    //                 .bodyToMono(String.class)
-    //                 .doOnTerminate(() -> cdl.countDown())
-    //                 .subscribe(res -> result.append(res));
-
-    //         cdl.await();
-    //     } catch (IOException | InterruptedException e) {
-    //         e.printStackTrace();
-    //     }
-
-    //     SendChatDto chat = new SendChatDto();
-    //     chat.setMessage(result.toString());
-    //     chat.setType("AUDIO");
-
-    //     return new MessageResponse(null, "200", chat);
-    // }
-
     @PostMapping("/upload")
-    public void uploadAudio(@ModelAttribute MultipartFile file) {
+    public MessageResponse uploadAudio(@ModelAttribute MultipartFile file) {
         CountDownLatch cdl = new CountDownLatch(1);
         StringBuilder result = new StringBuilder();
 
-        // Path filePath = Path.of("C:/Audio/" + UUID.randomUUID());
         try {
-            file.transferTo(new File("C:/Audio/" + UUID.randomUUID()));
-        } catch (IllegalStateException | IOException e) {
-            // TODO Auto-generated catch block
+            File temp = File.createTempFile(file.getOriginalFilename(), ".wav");
+            // 파일 저장
+            file.transferTo(new File(temp.getPath()));
+
+            WebClient.create()
+                    .post()
+                    .uri("http://localhost:8000/api/v1/audio/text/kor_Hang") // FastAPI 엔드포인트 경로
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(BodyInserters.fromMultipartData("file", new UrlResource(temp.toURI())))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .doOnTerminate(() -> cdl.countDown())
+                    .subscribe(res -> result.append(res));
+
+            cdl.await();
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
 
-        // try {
-        //     File temp = File.createTempFile(file.getOriginalFilename(), ".wav");
-        //     // 파일 저장
-        //     file.transferTo(new File(temp.getPath()));
+        SendChatDto chat = new SendChatDto();
+        chat.setMessage(result.toString());
+        chat.setType("AUDIO");
 
-        //     WebClient.create()
-        //             .post()
-        //             .uri("http://localhost:8000/api/v1/audio/text/kor_Hand") // FastAPI 엔드포인트 경로
-        //             .contentType(MediaType.MULTIPART_FORM_DATA)
-        //             .body(BodyInserters.fromMultipartData("file", new UrlResource(temp.toURI())))
-        //             .retrieve()
-        //             .bodyToMono(String.class)
-        //             .doOnTerminate(() -> cdl.countDown())
-        //             .subscribe(res -> result.append(res));
-
-        //     cdl.await();
-        // } catch (IOException | InterruptedException e) {
-        //     e.printStackTrace();
-        // }
-
-        // SendChatDto chat = new SendChatDto();
-        // chat.setMessage(result.toString());
-        // chat.setType("AUDIO");
-
-        // return new MessageResponse(null, "200", chat);
+        return new MessageResponse(null, "200", chat);
     }
+
+    // @PostMapping("/upload")
+    // public void uploadAudio(@ModelAttribute MultipartFile file) {
+    //     CountDownLatch cdl = new CountDownLatch(1);
+    //     StringBuilder result = new StringBuilder();
+
+    //     // Path filePath = Path.of("C:/Audio/" + UUID.randomUUID());
+    //     try {
+    //         file.transferTo(new File("C:/Audio/" + UUID.randomUUID()));
+    //     } catch (IllegalStateException | IOException e) {
+    //         // TODO Auto-generated catch block
+    //         e.printStackTrace();
+    //     }
+
+    //     // try {
+    //     //     File temp = File.createTempFile(file.getOriginalFilename(), ".wav");
+    //     //     // 파일 저장
+    //     //     file.transferTo(new File(temp.getPath()));
+
+    //     //     WebClient.create()
+    //     //             .post()
+    //     //             .uri("http://localhost:8000/api/v1/audio/text/kor_Hand") // FastAPI 엔드포인트 경로
+    //     //             .contentType(MediaType.MULTIPART_FORM_DATA)
+    //     //             .body(BodyInserters.fromMultipartData("file", new UrlResource(temp.toURI())))
+    //     //             .retrieve()
+    //     //             .bodyToMono(String.class)
+    //     //             .doOnTerminate(() -> cdl.countDown())
+    //     //             .subscribe(res -> result.append(res));
+
+    //     //     cdl.await();
+    //     // } catch (IOException | InterruptedException e) {
+    //     //     e.printStackTrace();
+    //     // }
+
+    //     // SendChatDto chat = new SendChatDto();
+    //     // chat.setMessage(result.toString());
+    //     // chat.setType("AUDIO");
+
+    //     // return new MessageResponse(null, "200", chat);
+    // }
 
     @PostMapping("/transport/{nationalLanguage}")
     public ResponseEntity<?> translateTextToAudio(@RequestBody SendChatDto chat,
