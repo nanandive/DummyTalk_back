2920d811003c34846a611c6e84c683ac60014ec8
Choi Youngwook <duddnr3618@gmail.com>
2023-12-27 16:44:20 +0900

    23.12.27 서버 ì삭제 권한 제한

--- src/main/java/com/example/DummyTalk/User/Repository/UserChatRepository.java
+++ src/main/java/com/example/DummyTalk/User/Repository/UserChatRepository.java
@@ -1,5 +1,7 @@
 package com.example.DummyTalk.User.Repository;
 
+import com.example.DummyTalk.Chat.Server.Entity.ServerEntity;
+import com.example.DummyTalk.User.Entity.User;
 import com.example.DummyTalk.User.Entity.UserChat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,7 @@ import java.util.Optional;
 public interface UserChatRepository extends JpaRepository<UserChat, Long> {
     List<UserChat> findByServerId(Long serverId);
 
+
+
+    void deleteByUserAndServer(User user, ServerEntity server);
 }
