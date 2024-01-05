ac181026803a1720329260349df8d515eccd9c75
kimjisu123 <dkljjh10@gmail.com>
2024-01-05 18:33:44 +0900

    커밋

--- src/main/java/com/example/DummyTalk/User/Repository/FriendRepository.java
+++ src/main/java/com/example/DummyTalk/User/Repository/FriendRepository.java
@@ -4,6 +4,7 @@ import com.example.DummyTalk.User.Entity.Friend;
 import com.example.DummyTalk.User.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,11 +15,11 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     @Query("SELECT f FROM Friend f " +
             "WHERE f.friendUserId = :userId " +
             "AND f.userId = :friendId")
-    Friend findByFriend(int userId, String friendId);
+    Friend findByFriend(@Param("userId")int userId, @Param("friendId")String friendId);
 
     @Query("SELECT f FROM Friend f " +
             "JOIN User u ON u.userId = f.userId " +
             "WHERE f.userId = :result")
-    List<Friend> findByUser(Long result);
+    List<Friend> findByUser(@Param("result")Long result);
 
 }
